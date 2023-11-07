bd78eebe4a5b60944447b8c74ab7b39be7d82bf3
ldehbi <149448333+ldehbi@users.noreply.github.com>
2023-11-06 18:24:30 -0600

    updated SQL file and ERD

--- TESTBANKDB_gen.sql
+++ TESTBANKDB_gen.sql
@@ -5,32 +5,13 @@ CREATE SCHEMA IF NOT EXISTS `testbank` DEFAULT CHARACTER SET utf8mb4 COLLATE utf
 USE `testbank` ;
 
 -- -----------------------------------------------------
--- Table `testbank`.`category`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `testbank`.`category` (
-  `category_id` INT NOT NULL,
-  `category_desc` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`category_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
 -- Table `testbank`.`questions`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `testbank`.`questions` (
   `qstn_id` INT NOT NULL,
   `qstn_description` VARCHAR(300) NULL DEFAULT NULL,
   `qstn_answer` INT NULL DEFAULT NULL,
-  `category_category_id` INT NOT NULL,
-  PRIMARY KEY (`qstn_id`, `category_category_id`),
-  INDEX `fk_questions_category1_idx` (`category_category_id` ASC) VISIBLE,
-  CONSTRAINT `fk_questions_category1`
-    FOREIGN KEY (`category_category_id`)
-    REFERENCES `testbank`.`category` (`category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`qstn_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -42,14 +23,27 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `testbank`.`answers` (
   `ans_id` INT NOT NULL,
   `ans_description` VARCHAR(300) NULL DEFAULT NULL,
-  `ans_qstn` INT NULL DEFAULT NULL,
   `ans_right` CHAR(1) NULL DEFAULT NULL,
   `questions_qstn_id` INT NOT NULL,
   PRIMARY KEY (`ans_id`, `questions_qstn_id`),
   INDEX `fk_answers_questions1_idx` (`questions_qstn_id` ASC) VISIBLE,
   CONSTRAINT `fk_answers_questions1`
     FOREIGN KEY (`questions_qstn_id`)
-    REFERENCES `testbank`.`questions` (`qstn_id`))
+    REFERENCES `testbank`.`questions` (`qstn_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `testbank`.`category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `testbank`.`category` (
+  `category_id` INT NOT NULL,
+  `category_desc` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`category_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -81,29 +75,41 @@ CREATE TABLE IF NOT EXISTS `testbank`.`tests` (
   INDEX `fk_tests_users_idx` (`users_user_id` ASC) VISIBLE,
   CONSTRAINT `fk_tests_users`
     FOREIGN KEY (`users_user_id`)
-    REFERENCES `testbank`.`users` (`user_id`))
+    REFERENCES `testbank`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `testbank`.`questions_has_tests`
+-- Table `testbank`.`tests_questions_rel`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `testbank`.`questions_has_tests` (
+CREATE TABLE IF NOT EXISTS `testbank`.`tests_questions_rel` (
   `questions_qstn_id` INT NOT NULL,
   `tests_test_id` INT NOT NULL,
   `tests_users_user_id` INT NOT NULL,
-  PRIMARY KEY (`questions_qstn_id`, `tests_test_id`, `tests_users_user_id`),
+  `category_category_id` INT NOT NULL,
+  PRIMARY KEY (`questions_qstn_id`, `tests_test_id`, `tests_users_user_id`, `category_category_id`),
   INDEX `fk_questions_has_tests_tests1_idx` (`tests_test_id` ASC, `tests_users_user_id` ASC) VISIBLE,
   INDEX `fk_questions_has_tests_questions1_idx` (`questions_qstn_id` ASC) VISIBLE,
+  INDEX `fk_questions_has_tests_category1_idx` (`category_category_id` ASC) VISIBLE,
   CONSTRAINT `fk_questions_has_tests_questions1`
     FOREIGN KEY (`questions_qstn_id`)
-    REFERENCES `testbank`.`questions` (`qstn_id`),
+    REFERENCES `testbank`.`questions` (`qstn_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_questions_has_tests_tests1`
     FOREIGN KEY (`tests_test_id` , `tests_users_user_id`)
-    REFERENCES `testbank`.`tests` (`test_id` , `users_user_id`))
+    REFERENCES `testbank`.`tests` (`test_id` , `users_user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_questions_has_tests_category1`
+    FOREIGN KEY (`category_category_id`)
+    REFERENCES `testbank`.`category` (`category_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
-

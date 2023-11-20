92dedde1b820c72fc2a95e7aee903ae78e0359b7
Tobi Wallace <twallace@catalyte.io>
2023-11-20 15:25:25 -0600

    continue working on generate and login page

--- app/generateExam.py
+++ app/generateExam.py
@@ -1,31 +1,5 @@
 from tkinter import *
-import regex as re
-import mysql.connector
-import argon2
 import tkinter.messagebox as msg
-import registration
-import generateExam
-
-# Database Integration
-conn = mysql.connector.connect(
-    host="localhost",  # hostname
-    user="Torres",  # username
-    password="Test123!",  # password
-    database="testbank",  # database name
-)  # please note the information above will need to be changed according the database in use
-
-cursor = conn.cursor()
-
-
-def button_clicked():
-    """
-    This function will submit the registatrion form,
-    Hash the password using argon2, and
-    it will notify user if registration was succesful or not.
-    """
-    email = email_input.get()
-    password = password_input.get()
-
 
 subject = [
 "Geology",
@@ -46,30 +20,40 @@ question_format = [
 "Fill-in-the-Blank"
 ] 
 
-# Initiate GUI
-ws = Tk()
-ws.title("Log-In Page")
-ws.geometry("400x200")
+def button_clicked():
+    """
+    This function will help display error messages
+    """
+    msg.showerror("Error")
 
-frame = Frame(ws, relief=SOLID, bd=2, padx=10, pady=10)
+
+# Initiate GUI
+
+ws = Tk()
+ws.title("CIS 245 Testbank Application")
+ws.geometry("500x500")
+
+
+frame = Frame(ws)
 
 # Creating Labels
-Label(frame, text="Subject:", padx=5, pady=5).grid(row=0, column=0, sticky=W)
-Label(frame, text="Difficulty:", padx=5, pady=5).grid(row=1, column=0, sticky=W)
-Label(frame, text="Number of Questions:", padx=5, pady=5).grid(row=2, column=0, sticky=W)
-Label(frame, text="Question Formats:", padx=5, pady=5).grid(row=3, column=0, sticky=W)
+Label(frame, text="Generate Exam", padx=15, pady=15).grid(row=0, column=1, sticky=W)
+Label(frame, text="Subject:", padx=5, pady=5).grid(row=1, column=0, sticky=W)
+Label(frame, text="Difficulty:", padx=5, pady=5).grid(row=2, column=0, sticky=W)
+Label(frame, text="Number of Questions:", padx=5, pady=5).grid(row=3, column=0, sticky=W)
+Label(frame, text="Question Formats:", padx=5, pady=5).grid(row=4, column=0, sticky=W)
 
 
 # Creating Dropdowns
 subjects = StringVar(ws)
 subjects.set(subject[0])
-subject_input = OptionMenu( ws , subjects , *subject )
+subject_input = OptionMenu( frame , subjects , *subject )
 difficulties = StringVar(ws)
 difficulties.set(difficulty[0])
-difficulty_input = OptionMenu( ws , difficulties , *difficulty ) 
-formats = StringVar(ws)
-formats.set(format[0])
-format_input = OptionMenu( ws , formats , *format ) 
+difficulty_input = OptionMenu( frame , difficulties , *difficulty ) 
+format_input = Listbox( frame, selectmode='multiple' ) 
+for format in question_format:
+    format_input.insert(question_format.index(format)+1, format)
 
 # Creating entry for # of questions
 questionnumber_input = Entry(frame)
@@ -84,12 +68,12 @@ logout_button.bind("<Return>", lambda event: button_clicked())
 
 
 # Placement of text boxes and buttons
-logout_button.grid(row=0, column=1, padx=10, pady=10)
+logout_button.grid(row=0, column=0, padx=10, pady=10)
 subject_input.grid(row=1, column=1, padx=10, pady=10)
 difficulty_input.grid(row=2, column=1, padx=10, pady=10)
 questionnumber_input.grid(row=3, column=1, padx=5, pady=5)
 format_input.grid(row=4, column=1, padx=10, pady=10)
-generateExam_button.grid(row=5, column=1, padx=10, pady=10)
+generateExam_button.grid(row=5, column=0, padx=10, pady=10)
 
 frame.pack()
 
--- app/login.py
+++ app/login.py
@@ -1,32 +1,11 @@
 from tkinter import *
-import regex as re
-import mysql.connector
-import argon2
 import tkinter.messagebox as msg
-import registration
-import generateExam
 
-# Database Integration
-conn = mysql.connector.connect(
-    host="localhost",  # hostname
-    user="Torres",  # username
-    password="Test123!",  # password
-    database="testbank",  # database name
-)  # please note the information above will need to be changed according the database in use
-
-cursor = conn.cursor()
-
-# Argon2 password hasher
-password_hasher = argon2.PasswordHasher()
-
-
-# Swith to registration page link
-def switch_to_registration():
-    registration
 
 # Swith to exam generation page link
 def switch_to_generateExam():
-    generateExam
+    ws.destroy()
+    import generateExam
 
 
 # Immediate Feedback on Errors
@@ -36,63 +15,53 @@ def display_error(message):
     """
     msg.showerror("Error", message)
 
-# Verifies that username & password match
-def login_validator(email, password):
-    if len(password) >= 8:
-        if (
-            re.search(r"[!@#$%^&*()_+]", password)
-            and re.search(r"\d", password)
-            and len(password) <= 50
-        ):
-            return None
-        else:
-            return "Password must include at least one special character and one number and cannot be longer than 50 characters."
-    else:
-        return "Password must be at least 8 characters."
 
-
-def button_clicked():
-    """
-    This function will submit the registatrion form,
-    Hash the password using argon2, and
-    it will notify user if registration was succesful or not.
-    """
+def login_validation():
     email = email_input.get()
     password = password_input.get()
 
-    login_error = login_validator(email, password)
+    if email and password:
+        # Verifies that username & password match
+        # Right now there's no additional validation or seperate errors for a missing username/password
+        if email == "test@gmail.com" and password == "password":
+            switch_to_generateExam()
+        else:
+            display_error("Incorrect email and/or password")
+    
 
-    if login_error:
-        display_error(login_error)
-    else:
-        # navigate to generateExam.py
-        display_error(login_error)
+def load_registration():
+    ws.destroy()
+    import generateExam
 
 
 # Initiate GUI
 ws = Tk()
-ws.title("Log-In Page")
-ws.geometry("400x200")
+ws.title("CIS 245 Testbank Application")
+ws.geometry("500x500")
 
-frame = Frame(ws, relief=SOLID, bd=2, padx=10, pady=10)
+frame = Frame(ws)
 
 # Creating Labels
-Label(frame, text="Email:", padx=5, pady=5).grid(row=0, column=0, sticky=W)
-Label(frame, text="Password:", padx=5, pady=5).grid(row=1, column=0, sticky=W)
+Label(frame, text="Log In", padx=15, pady=15).grid(row=0, column=0, sticky=W)
+Label(frame, text="Email:", padx=5, pady=5).grid(row=1, column=0, sticky=W)
+Label(frame, text="Password:", padx=5, pady=5).grid(row=2, column=0, sticky=W)
 
 # Creating text boxes
 email_input = Entry(frame)
 password_input = Entry(frame, show="*")
 
 # Creating login button
-login_button = Button(frame, text="Log In", command=button_clicked, cursor="mouse")
-login_button.bind("<Return>", lambda event: button_clicked())
+login_button = Button(frame, text="Log In", command= login_validation, cursor="mouse")
+login_button.bind("<Return>", lambda event: login_validation)
+registration_button = Button(frame, text="Create Account", command= load_registration, cursor="mouse")
+registration_button.bind("<Return>", lambda event: load_registration)
 
 
 # Placement of text boxes and buttons
-email_input.grid(row=0, column=1, padx=5, pady=5)
-password_input .grid(row=1, column=1, padx=5, pady=5)
+email_input.grid(row=1, column=1, padx=5, pady=5)
+password_input.grid(row=2, column=1, padx=5, pady=5)
 login_button.grid(row=3, column=1, padx=10, pady=10)
+registration_button.grid(row=4, column=1, padx=10, pady=10)
 
 frame.pack()
 
